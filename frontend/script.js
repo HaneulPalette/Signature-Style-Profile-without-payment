937c2874dd439629d6e92c3afd0cde18ce00bd24
HaneulPalette <haneulpalette@gmail.com>
2026-04-28 21:48:29 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-
-function analyze() {
-    document.getElementById("preview").innerText =
-        "Preview: Warm tone detected (demo frontend only)";
-}
-
-function downloadPDF() {
-    alert("Backend needed for PDF generation");
-}
