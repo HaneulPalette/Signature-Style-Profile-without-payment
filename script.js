cae23fd1037e2e85ff732416251f7f709183031b
HaneulPalette <haneulpalette@gmail.com>
2026-04-29 11:59:18 +0530

    Add initial styles for the web application

---
+++ script.js
@@ -0,0 +1,37 @@
+body {
+    font-family: Arial;
+    background: #f6f4ef;
+    text-align: center;
+}
+
+.container {
+    margin-top: 60px;
+}
+
+.logo {
+    width: 90px;
+}
+
+input {
+    margin: 20px;
+}
+
+button {
+    padding: 12px 20px;
+    background: black;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+.card {
+    background: white;
+    padding: 20px;
+    display: inline-block;
+    margin-top: 20px;
+    border-radius: 10px;
+}
+
+.hidden {
+    display: none;
+}
